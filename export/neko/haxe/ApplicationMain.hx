6b1a4d7585d975d2f5e8f008269de4c7858fb2c9
LilDrippyMyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-07-09 08:38:10 -0400

    export things

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "2";
+		app.meta["build"] = "3";
 		app.meta["company"] = "MyFnf";
 		app.meta["file"] = "FREZY";
 		app.meta["name"] = "Frezy Friday' ";
